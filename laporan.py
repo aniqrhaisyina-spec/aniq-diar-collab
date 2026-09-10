6ecae28a97cb6fb4f4e81215be9cd08fb2283f50
diarnopitasari <diarnopitasari@apps.ipb.ac.id>
2026-09-10 16:53:24 +0000

    Menambahkan fungsi laporan hasil panen

---
+++ laporan.py
@@ -0,0 +1,6 @@
+def laporan_hasil_panen(hasil_panen):
+    total = sum(hasil_panen)
+
+    print("===== LAPORAN HASIL PANEN =====")
+    print("Data hasil panen:", hasil_panen)
+    print("Total hasil panen:", total, "kg")
